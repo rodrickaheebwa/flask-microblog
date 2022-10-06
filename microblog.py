f881e58b7b234a5f3d54bb0ef4608ac7dde8c12e
rodrickaheebwa <rodrickaheebwa@gmail.com>
2022-10-06 12:03:50 +0300

    set shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+
+from app.models import User, Post
+
+# setting shell context to add our database instance for testing purposes as we build our app
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
