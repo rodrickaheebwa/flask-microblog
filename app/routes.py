1e818a8d4f6619cd9d22d13d96ed7195930406d1
rodrickaheebwa <rodrickaheebwa@gmail.com>
2022-10-19 10:07:07 +0300

    added posts form to home page

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,9 @@ from flask import request
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 
 @app.before_request
 def before_request():
@@ -14,11 +14,17 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username' : 'Rodrick'}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -29,7 +35,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
